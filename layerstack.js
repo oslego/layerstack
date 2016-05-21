eadd66dc53d6f6bbadba939825035c27e42855cb
Razvan Caliman <razvan.caliman@gmail.com>
2016-05-21 19:41:29 +0100

    overal prototype improvements

--- layerstack.js
+++ layerstack.js
@@ -7,51 +7,92 @@
   Visualize in 3D.
 */
 function LayerStack(el) {
+
+  // Ensure we're called with `new`
+  if (!(this instanceof LayerStack)) {
+    return new LayerStack(el);
+  }
+
+  // Helpers
   var _qs = document.querySelector.bind(document);
 
+  // Store for event callbacks
+  var _events = {};
+
   if (!el) {
-    throw Error('Missing input element');
+    throw TypeError(`Invalid element parameter. Expected DOM element, got ${el}.`);
     return;
   }
 
   function getBackgroundLayers(el) {
-    var style = window.getComputedStyle(el)
+    var style = window.getComputedStyle(el);
 
-    // TODO: add support for separate border declarations
-    var border = style.getPropertyValue('border');
+    /*
+    Create array of objects with layer data from `background-image` value.
 
-    var images = style.getPropertyValue('background-image');
+    Add an extra paranthesis at end of each image declaration,
+    then split by one paranthesis and comma to yield an array of declarations.
 
-    /* Add an extra paranthesis at end of each gradient declaration,
-    then split by one paranthesis and comma to yield an array of valid
-    gradient declarations.
+    If there is no backgrond-image value, the computed value is `none`.
+    That yields an array with a single value onto which we can tack on stuff
+    like `background-color`, if defined.
     */
-    images = images.replace(/\),/gi, ')),').split('),');
+    layers = style.getPropertyValue('background-image')
+              .replace(/\),/gi, ')),')
+              .split('),')
+              .map(function(img) {
+                return {
+                  'background-image': img,
+                  'box-sizing': style.getPropertyValue('box-sizing'),
+                  // TODO: add support for separate border declarations
+                  'border': style.getPropertyValue('border')
+                }
+              });
 
-    var layers = images.map(function(img) {
-      return { 'background-image': img }
-    });
-
-    // All other bg properties can be split by comma.
+    // All other background properties can be split by comma.
     var props = ['background-size', 'background-repeat', 'background-position', 'background-origin'];
-    props.forEach(function(prop) {
-      style.getPropertyValue(prop).split(',').forEach(function(value, index) {
-        layers[index][prop] = value.trim();
-        layers[index]['border'] = border;
 
-        // TODO find actual box sizing
-        layers[index]['box-sizing'] = 'border-box';
-      })
+    props.forEach(function(prop) {
+      var values = style.getPropertyValue(prop).split(',');
+
+      /*
+        If there's one background-* value, like background-repeat, but multiple background images,
+        getGomputedStyle() in Firefox won't replicate the value to match the number of images.
+        Do this manually.
+      */
+      if (values.length === 1) {
+        layers.forEach(function(layer) {
+          layer[prop] = values[0].trim();
+        })
+      } else {
+        values.forEach(function(value, index) {
+          layers[index][prop] = value.trim();
+        })
+      }
     })
 
-    console.log(layers)
+    // Use backgrond-color only on base layer
+    layers[layers.length - 1]['background-color'] = style.getPropertyValue('background-color');
 
     return layers;
   }
 
+  function getTransformStyleSheetForLayers(layers) {
+    var style = document.createElement('style');
+    // offset step between layers
+    var step = 75;
+
+    layers.forEach(function(layer, index) {
+      // Concat transform style for this layer
+      style.textContent += `.animated .bg-stack__layer:nth-child(${index + 1}) {
+        transform: translateY(calc(var(--yoffset, 0) * ${index * -1 * step}px)) translateZ(calc(var(--yoffset, 0) * ${index * step}px));
+      }\n`
+    })
+
+    return style;
+  }
+
   var sourceEl = el;
-  var sourceElBox = sourceEl.getBoundingClientRect();
-  var sourceElBgLayers = getBackgroundLayers(sourceEl);
 
   var stack = document.createElement('div');
   stack.classList.add('bg-stack');
@@ -60,9 +101,11 @@ function LayerStack(el) {
   stackContainer.classList.add('bg-stack-container');
   stackContainer.appendChild(stack);
 
-  sourceElBgLayers.forEach(function(layer) {
+  var sourceElBgLayers = getBackgroundLayers(sourceEl);
+  sourceElBgLayers.forEach(function(layer, index) {
     var stackLayer = document.createElement('div')
     stackLayer.classList.add('bg-stack__layer');
+
     for (var prop in layer){
       stackLayer.style[prop] = layer[prop];
     }
@@ -71,11 +114,16 @@ function LayerStack(el) {
     stack.insertBefore(stackLayer, stack.firstChild)
   })
 
+  var sourceElBox = sourceEl.getBoundingClientRect();
   for (var key in sourceElBox) {
     stackContainer.style[key] = sourceElBox[key] + 'px';
   }
 
+  var transformStyleSheet = getTransformStyleSheetForLayers(sourceElBgLayers);
+  document.head.appendChild(transformStyleSheet);
+
   sourceEl.parentElement.appendChild(stackContainer);
+  sourceEl.classList.add('u-invisible');
 
   // Add animated class after appending to DOM so we get smooth transition
   requestAnimationFrame(function(){
@@ -119,18 +167,28 @@ function LayerStack(el) {
 
   document.addEventListener('mousemove', update);
 
-  var _events = {};
-
   return {
+    /*
+      Remove LayerStack DOM nodes and styling. Restore visibility of source object.
+      @param {Object} options - config object for destroy action.
+
+      Config options:
+      {Boolean} immediate - When true, destroy without running animation. Default `false`.
+
+      @example destroy({ immediate: true })
+    */
     destroy: function(options){
       document.removeEventListener('mousemove', update);
       stackContainer.classList.remove('animated');
       var self = this;
 
       function destroyDOM(e) {
+        transformStyleSheet.parentElement.removeChild(transformStyleSheet);
         cssVars.parentElement.removeChild(cssVars);
         stackContainer.removeEventListener('transitionend', destroyDOM)
         stackContainer.parentElement.removeChild(stackContainer);
+
+        sourceEl.classList.remove('u-invisible');
         self.trigger('afterdestroy');
       }
 
